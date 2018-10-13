04dc45decdbc4c3376afb516c082efe7ea3a0dde
natziv <natasaziv@gmail.com>
2018-10-13 12:30:56 +0200

    My Django Girls app, first commit

---
+++ djangogirls_python_intro.py
@@ -0,0 +1,10 @@
+def hi(name):
+	print('Hi ' + name + '!' + ' How are you?')
+
+girls = ['Alex', 'Sam', 'Libby','You']
+for name in girls:
+	hi(name)
+	print('I am great! Thanks :)')
+
+for i in range (1,14):
+	print(i)
